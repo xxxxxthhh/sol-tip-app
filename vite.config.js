eaea103a8794628dc1bbe87fdef610b2e5419950
xxxxxthhh <xxxxxthhh@users.noreply.github.com>
2026-02-12 14:49:26 +0800

    feat: add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/sol-tip-app/',
   plugins: [react()],
   define: {
     'process.env': {},
